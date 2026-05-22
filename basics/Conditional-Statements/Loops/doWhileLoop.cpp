e12a043450d6a963635f21506525271fafa66ccb
Darsan <mannemdarsan@gmail.com>
2026-05-22 17:16:13 +0530

    This is all about do while loop.

---
+++ basics/Conditional-Statements/Loops/doWhileLoop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int num = 10;
+    int i = 1;
+    do{
+        cout << i << " ";
+        i++;
+    }while (i <=num);
+
+    return 0;
+}
