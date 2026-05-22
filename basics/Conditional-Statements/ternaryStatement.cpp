7d6aa28b642d941be64a44d7ee29aa05af6c0f18
Darsan <mannemdarsan@gmail.com>
2026-05-22 15:05:09 +0530

    This is all about Ternary Statement(condition ? stt1: strr2;).

---
+++ basics/Conditional-Statements/ternaryStatement.cpp
@@ -0,0 +1,14 @@
+// condition ? stt1: strr2;
+// It is simple if-else
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    
+    cout << "Enter any number :" ;
+    cin >> n ;
+
+    cout << (n>=0 ?"positive":"negative") ;
+    return 0;
+}
