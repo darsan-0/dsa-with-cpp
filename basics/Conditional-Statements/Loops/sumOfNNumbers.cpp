750d1235f06356064ca3833c9c15bb10deb24c51
Darsan <mannemdarsan@gmail.com>
2026-05-22 17:10:37 +0530

    This program about Sum of n numbers & sum of odd numbers upto n.

---
+++ basics/Conditional-Statements/Loops/sumOfNNumbers.cpp
@@ -0,0 +1,24 @@
+// Sum of N +ve snumbers
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int num,sum = 0 ;
+    int oddSum = 0;
+    cout << "Enter the value of num: " << endl;
+    cin >> num ;
+    // Sum of n numbers
+    for (int i=1 ; i <=num ; i++){
+        sum += i;
+    }
+    // sum of odd numbers upto n
+    for (int i=1 ; i <=num ; i++){
+        if (i%2 != 0){
+            oddSum += i;
+        }
+    }
+    cout << "The sum of " << num << " numbers is :" << sum;
+    cout << "The sum of odd numbers upto " << num << " is :" << oddSum;
+    return 0;
+}
