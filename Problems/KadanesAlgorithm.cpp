c242112df03044959341c9e544f2fd5e0e52a206
Darsan <mannemdarsan@gmail.com>
2026-05-27 19:48:48 +0530

    This Kadanes Algorithm is the best approch to find the maximum sum of sub arrray dude.

--- Problems/KadanesAlgorithm.cpp
+++ Problems/KadanesAlgorithm.cpp
@@ -4,6 +4,10 @@ using namespace std;
 
 int main(){
 
+//     Choice 1: Extend the maximum sum subarray ending at the previous element by adding the current element to it. If the maximum subarray sum ending at the previous index is positive, then it is always better to extend the subarray.
+
+// Choice 2: Start a new subarray starting from the current element. If the maximum subarray sum ending at the previous index is negative, it is always better to start a new subarray from the current element.
+
     vector <int> vec = {3,-4,5,4-1,7,-8};
     int n = vec.size();
     int maxsum = INT_MIN;
