25658a021a298e48eff363360eadfc64a0b1e686
Darsan <mannemdarsan@gmail.com>
2026-05-27 19:36:47 +0530

    This is all about maximum_subarray_sum_brute_force.

---
+++ Problems/maximum_subarray_sum_brute_force.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+
+    vector <int> vec = {3,-4,5,4-1,7,-8};
+    int n = vec.size();
+    int sum = INT_MIN;
+    for(int st = 0; st < n; st++ ){
+        int res = 0;
+        for(int end = st; end < n; end++){
+            res += vec[end];
+            sum = max(sum,res);
+        }
+    }
+    cout <<"Maximum subarray sum = " << sum;
+
+    return 0;
+}
