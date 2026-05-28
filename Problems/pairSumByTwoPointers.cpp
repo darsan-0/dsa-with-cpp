382c418bf798d13c6a38efa99fb119a3ed9480e9
Darsan <mannemdarsan@gmail.com>
2026-05-28 10:10:01 +0530

    This is all about pair Sum with 2 pointers approch.

---
+++ Problems/pairSumByTwoPointers.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector <int> pairSum(vector <int> vec,int target){
+
+    int ps = 0;
+    int n = vec.size() - 1;
+    int i = 0;
+    int j = n - 1;
+    vector <int> ans;
+    while (i < j){
+        ps = vec[i] + vec[j];
+        if(ps == target){
+            ans.push_back(i);
+            ans.push_back(j);
+            return ans;
+        }else if (ps > target){
+            j--;
+        }else{
+            i++;
+        }
+    }
+    return ans;
+}
+
+int main(){
+
+    vector <int> vec = { 2,7,11,15};
+    int target = 18;
+    vector <int> ans = pairSum(vec , target);
+    cout << ans[0] << "," << ans[1];
+    return 0;
+}
